ca2bbd597dc0f3041f4b50fcf3a411ad5c068e3a
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-08 20:09:01 +0300

    updated help command

--- built_in_commands.c
+++ built_in_commands.c
@@ -8,19 +8,29 @@ int built_in_checker(char *cmd)
 
             exit(0);
 	}
+	if (strcmp(cmd, "clear\n") == 0)
+	{
+		system("clear");
+		return (1);
+	}
 	 if (strcmp(cmd, "help\n") == 0)
 	{
-		_puts("Built-in commands:\n");
-		_puts("help - display this help\n");
-		_puts("ls - display the files in the current directory\n");
-		_puts("mkdir - create directory\n");
-		_puts("pwd - display the current working directory\n");
-		_puts("cd - change directory\n");
-		_puts("rm - remove file\n");
-		_puts("rmdir - remove directory\n");
-		_puts("env - display environment\n");
-		_puts("clear - clear the screen\n");
-		_puts("exit - exit the shell\n");
+		_puts("\n\n\t****************************************************************\n");
+        _puts("\t***************** |   Simple bash shell    | *******************\n");
+        _puts("\t****************************************************************\n");
+        _puts("\tDeveloped by Biruk Yonas & Getie \n");
+        _puts("\trepo : `https://github.com/buka-pitch/simple_shell`\n\n");
+		_puts("\tBuilt-in commands:\n");
+		_puts("\t\thelp - display this help\n");
+		_puts("\t\tls - display the files in the current directory\n");
+		_puts("\t\tmkdir - create directory\n");
+		_puts("\t\tpwd - display the current working directory\n");
+		_puts("\t\tcd - change directory\n");
+		_puts("\t\trm - remove file\n");
+		_puts("\t\trmdir - remove directory\n");
+		_puts("\t\tenv - display environment\n");
+		_puts("\t\tclear - clear the screen\n");
+		_puts("\t\texit - exit the shell\n");
 		return (1);
 	}
 	return (0);
