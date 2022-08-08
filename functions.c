07b867d3c7b405e167c67c1d7cae73fd3314ccdd
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-08 20:10:01 +0300

    updated initial message

--- functions.c
+++ functions.c
@@ -1,11 +1,21 @@
 #include "shell.h"
 
 void prompt(){
+    static int init = 1;
+    if (init)
+    {
+        system("clear");
+        _puts("\n\t****************************************************************\n");
+        _puts("\n\t****************  -------------------------- **********************\n");
+        _puts("\t*****************|    Simple bash shell     | *******************\n");
+        _puts("\n\t***************** -------------------------- **********************\n");
+        _puts("\tDeveloped by Biruk Yonas & Getie \n");
+        _puts("\trepo : `https://github.com/buka-pitch/simple_shell`\n\n");
+    }
     char *buf = "$ ";
+    init = 0;
     if (isatty(STDIN_FILENO))
         _puts(buf);
-
-
 }
 
 int _putchar(char c)
